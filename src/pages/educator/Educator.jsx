b18b149ae13d8070350caec23aca982783a6d784
TEAtech-TBKs <tolulopeakinwale955@gmail.com>
2025-12-10 10:36:37 +0100

    fixed

--- src/pages/educator/Educator.jsx
+++ src/pages/educator/Educator.jsx
@@ -10,8 +10,8 @@ const Educator = () => {
       <Outlet />
     </div>
 
-    <div className="underconst flex fixed left-8 bottom-1/2 w-full">
-      <h1 className='text-gray-300 bg-violet-50 border-4 py-3 px-5 font-semibold mx-auto my-auto rounded text-5xl'>
+    <div className="underconst flex fixed left-8  bottom-1/2 w-full ">
+      <h1 className=' text-gray-500 bg-violet-50 border-4 py-3 px-5 font-semibold mx-auto my-auto rounded text-5xl'>
           Work in progress... <br /> This page is still being developed
       </h1>
     </div>
